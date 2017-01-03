2bad79f0ee56aec9952cfd85d3f859e4fb028933
James <jbean46@gmail.com>
2017-01-03 14:02:42 +0100

    Generate project

---
+++ Articulations/Articulation.swift
@@ -0,0 +1,29 @@
+//
+//  Articulation.swift
+//  Articulations
+//
+//  Created by James Bean on 1/3/17.
+//
+//
+
+public struct Articulation: OptionSet {
+    
+    // MARK: - Instance Properties
+    
+    /// Raw value.
+    public var rawValue: Int
+    
+    // MARK: - Initializers
+    
+    /// Create an `Articulation` with a given `rawValue`.
+    public init(rawValue: Int) {
+        self.rawValue = rawValue
+    }
+    
+    public static let staccato = Articulation(rawValue: 1 << 0)
+    public static let staccatissimo = Articulation(rawValue: 1 << 1)
+    public static let tenuto = Articulation(rawValue: 1 << 2)
+    public static let accent = Articulation(rawValue: 1 << 3)
+    
+    // TODO: More
+}
