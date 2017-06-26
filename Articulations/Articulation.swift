c04e0a56bc2d1368971f2ca3d4c1f54aeb0cf7fa
Brian Heim <brianlheim@gmail.com>
2017-06-25 21:28:35 -0400

    Remove trailing ws

--- Articulations/Articulation.swift
+++ Articulations/Articulation.swift
@@ -7,23 +7,23 @@
 //
 
 public struct Articulation: OptionSet {
-    
+
     // MARK: - Instance Properties
-    
+
     /// Raw value.
     public var rawValue: Int
-    
+
     // MARK: - Initializers
-    
+
     /// Create an `Articulation` with a given `rawValue`.
     public init(rawValue: Int) {
         self.rawValue = rawValue
     }
-    
+
     public static let staccato = Articulation(rawValue: 1 << 0)
     public static let staccatissimo = Articulation(rawValue: 1 << 1)
     public static let tenuto = Articulation(rawValue: 1 << 2)
     public static let accent = Articulation(rawValue: 1 << 3)
-    
+
     // TODO: More
 }
