236bf025667244d00e5ac1da32c5e12dc24582fb
James <jbean46@gmail.com>
2017-07-02 15:52:25 +0200

    Add tests so XCTest doesn't fail, make sure tests see framework

---
+++ ArticulationsTests/ArticulationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArticulationTests.swift
+//  Articulations
+//
+//  Created by James Bean on 7/2/17.
+//
+//
+
+import XCTest
+import Articulations
+
+class ArticulationTests: XCTestCase {
+
+    func testArticulations() {
+        let articulations: [Articulation] = [.staccato, .staccatissimo, .tenuto, .accent]
+    }
+}
